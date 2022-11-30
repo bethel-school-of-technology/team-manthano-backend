32a922da11740e0ba32107525f738f63ae4611f4
Oscar Medrano <oscarsfolder@Oscars-MBP.lan>
2022-11-29 20:47:40 -0600

    Models folder created with started VehicleSchema

---
+++ api/models/Vehicle.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const VehicleSchema = new Schema ({
+
+    //Elements that we will be using
+})
+
+const Vehicle = mongoose.model("Vehicle, VehicleSchema");
+
+module.exports = Vehicle;
