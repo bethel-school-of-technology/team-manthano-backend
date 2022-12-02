edfb5459936cfa39af5eb20999efd2bc3b503f29
Oscar Medrano <oscarsfolder@Oscars-MBP.lan>
2022-12-01 19:47:20 -0600

    Created user and vehicle starting controller

---
+++ api/src/controllers/userController.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from "express";
+import { Users, IUsers } from "../models/User";
+
+export const createUser: RequestHandler = async (req, res, next) => {
+
+}
+
+export const loginUser: RequestHandler = async (req, res, next) => {
+
+}
---
+++ api/src/controllers/vehicleController.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import { Vehicles, IVehicles } from "../models/Vehicle";
+import { IUsers } from "../models/User";
+
+export const getAllVehicles: RequestHandler = async (req, res, next) => {
+    
+}
+
+export const getOneVehicle: RequestHandler = async (req, res, next) => {
+    
+}
+
+export const addVehicle: RequestHandler = async (req, res, next) => {
+   
+}
+
+export const editVehicle: RequestHandler = async (req, res, next) => {
+    
+}
+
+export const deleteVehicle: RequestHandler = async (req, res, next) => {
+   
+}
