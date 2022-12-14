92963ec84aea3591357a9a65c98f21374911a541
Jacob Imig <jacobimig9@gmail.com>
2022-12-14 12:04:45 -0600

    Tested Backend Routes

--- api/build/app.js
+++ api/build/app.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const morgan_1 = __importDefault(require("morgan"));
+const vehicleRoutes_1 = __importDefault(require("./routes/vehicleRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const connectionString = 'mongodb://localhost:27017/carShop';
 mongoose_1.default.connect(connectionString).then(() => console.log('database connection successful!'), err => console.log('Error connecting to the database', err));
 const app = (0, express_1.default)();
@@ -19,6 +21,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 // routes
+app.use('/api/vehicles', vehicleRoutes_1.default);
+app.use('/api/users', userRoutes_1.default);
 app.use((req, res, next) => {
     res.status(404).end();
 });
--- api/build/controllers/vehicleController.js
+++ api/build/controllers/vehicleController.js
@@ -1,18 +1,77 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteVehicle = exports.editVehicle = exports.addVehicle = exports.getOneVehicle = exports.getAllVehicles = void 0;
+const Vehicle_1 = require("../models/Vehicle");
+const auth_1 = require("../services/auth");
 const getAllVehicles = async (req, res, next) => {
+    let vehicleList = await Vehicle_1.Vehicles.find();
+    res.status(200).json(vehicleList);
 };
 exports.getAllVehicles = getAllVehicles;
 const getOneVehicle = async (req, res, next) => {
+    let itemId = req.params.id;
+    let vehicle = await Vehicle_1.Vehicles.findById(itemId);
+    res.status(200).json(vehicle);
 };
 exports.getOneVehicle = getOneVehicle;
 const addVehicle = async (req, res, next) => {
+    let user = await (0, auth_1.verifyUser)(req);
+    if (!user) {
+        return res.status(403).send();
+    }
+    const newVehicle = new Vehicle_1.Vehicles({
+        Name: req.body.Name,
+        Vehicle_Manufacturer: req.body.Vehicle_Manufacturer,
+        Year: req.body.Year,
+        Condition: req.body.Condition,
+        Price: req.body.Price,
+        Mileage: req.body.Mileage,
+        Body_Style: req.body.Body_Style,
+        Fuel_Economy: req.body.Fuel_Economy,
+        Exterior_Color: req.body.Exterior_Color,
+        Images: req.body.Images,
+        Posted_At: req.body.Posted_At
+    });
+    try {
+        await newVehicle.save();
+        res.status(201).json(newVehicle);
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
 };
 exports.addVehicle = addVehicle;
 const editVehicle = async (req, res, next) => {
+    let user = await (0, auth_1.verifyUser)(req);
+    if (!user) {
+        return res.status(403).send();
+    }
+    let itemId = req.params.id;
+    const updatedVehicle = new Vehicle_1.Vehicles({
+        _id: itemId,
+        Name: req.body.Name,
+        Vehicle_Manufacturer: req.body.Vehicle_Manufacturer,
+        Year: req.body.Year,
+        Condition: req.body.Condition,
+        Price: req.body.Price,
+        Mileage: req.body.Mileage,
+        Body_Style: req.body.Body_Style,
+        Fuel_Economy: req.body.Fuel_Economy,
+        Exterior_Color: req.body.Exterior_Color,
+        Images: req.body.Images,
+        Posted_At: req.body.Posted_At
+    });
+    await Vehicle_1.Vehicles.findByIdAndUpdate(itemId, { $set: updatedVehicle });
+    res.status(200).json(updatedVehicle);
 };
 exports.editVehicle = editVehicle;
 const deleteVehicle = async (req, res, next) => {
+    let user = await (0, auth_1.verifyUser)(req);
+    if (!user) {
+        return res.status(403).send();
+    }
+    let itemId = req.params.id;
+    let result = await Vehicle_1.Vehicles.findByIdAndDelete(itemId);
+    res.status(200).json(result);
 };
 exports.deleteVehicle = deleteVehicle;
