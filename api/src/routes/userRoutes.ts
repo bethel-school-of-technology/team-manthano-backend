6fd8bc0f21cac3c17b9134190bb0b7554b57244c
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-01-19 12:52:20 -0500

    fixed a bug with routing

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -6,10 +6,10 @@ const router = Router();
 router.post('/', createUser);
 router.post('/signin', loginUser);
 router.get('/', getUsers);
-router.get('/:id', getOneUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 router.get('/oneUser', getUser)
+router.get('/:id', getOneUser);
 router.get('/:id/messages', getUserMessages)
 router.put('/:id/messages', postUserMessage)
 
